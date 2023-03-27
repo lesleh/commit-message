ea3ba65c91c0bfbb20635f7169cb017ad087529a
Leslie Hoare <iam@lesleh.co.uk>
2023-03-27 11:37:56 +0100

    Refactor index.mjs: Add CommitMessageGenerator class
    Move the commit message generation logic into a new
    CommitMessageGenerator class to improve separation of concerns and
    modularity. The class now fetches the diff and creates the commit
    message using GPT-4 chat completion.

--- index.mjs
+++ index.mjs
@@ -12,34 +12,53 @@ const openai = new OpenAIApi(
   })
 );
 
-const completion = await openai.createChatCompletion({
-  model: "gpt-4",
-  messages: [
-    {
-      role: "system",
-      content: [
-        "You are a commit message creator. You will create a git commit message from the diff using the following format:",
-        "",
-        "[Commit title, no more than 50 characters]",
-        "",
-        "[Longer commit description, split at 72 characters]",
-      ].join("\n"),
-    },
-    {
-      role: "user",
-      content:
-        "Can you create a git commit message from a diff that I provide? Just reply with the actual commit message. Do not include the diff itself.",
-    },
-    {
-      role: "assistant",
-      content:
-        "Sure, please provide me with the diff and I can create the commit message for you.",
-    },
-    {
-      role: "user",
-      content: diff,
-    },
-  ],
-});
+class CommitMessageGenerator {
+  #openai;
 
-console.log(completion.data.choices[0].message.content);
+  constructor(openai) {
+    this.#openai = openai;
+  }
+
+  #getDiff() {
+    return execAsync("git diff");
+  }
+
+  async generateCommitMessage() {
+    const { stdout: diff } = await this.#getDiff();
+
+    const completion = await this.#openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "system",
+          content: [
+            "You are a commit message creator. You will create a git commit message from the diff using the following format:",
+            "",
+            "[Commit title, no more than 50 characters]",
+            "",
+            "[Longer commit description, split at 72 characters]",
+          ].join("\n"),
+        },
+        {
+          role: "user",
+          content:
+            "Can you create a git commit message from a diff that I provide? Just reply with the actual commit message. Do not include the diff itself.",
+        },
+        {
+          role: "assistant",
+          content:
+            "Sure, please provide me with the diff and I can create the commit message for you.",
+        },
+        {
+          role: "user",
+          content: diff,
+        },
+      ],
+    });
+
+    return completion.data.choices[0].message.content;
+  }
+}
+
+const generator = new CommitMessageGenerator(openai);
+console.log(await generator.generateCommitMessage());
